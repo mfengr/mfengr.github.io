4f5c18a85a4aa748887d2d71906b840b9aa4af6c
mfengr <mfengineeringplc@hotmail.com>
2024-09-23 11:18:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function hideSideBar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
+
+const p = document.createElement('p');
+p.textContet='Height='+scrollHeight;
+document.body.appendChild(p);
