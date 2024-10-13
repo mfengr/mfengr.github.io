a84810d4b8dd66018ffdbfdb753b16222b54ff8d
mfengr <mfengineeringplc@hotmail.com>
2024-10-13 20:20:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ for(let i=0;i<degreeTitles.length;i++){
      button.id = text;
      degreeTitles[i].appendChild(button);
 }
+
+
+
+function toggleFavorite(event) {
+ const target = event.target;
+  if (target.nodeName === 'BUTTON') {
+    event.preventDefault();
+    
+    if (target.textContent.startsWith('Add')) {
+         event.target.textContent = 'Remove from favorites';
+         const fav = document.createElement('p');
+         fav.textContent = '⭐ ⭐ ⭐ ';
+
+         target.parentElement.appendChild(fav);
+    } else {
+         event.target.textContent = 'Add to favorites';
+         target.nextSibling.remove();
+     }
+  }
+}
+
+const ndsContainer = document.querySelector('.list-nanodegrees');
+ndsContainer.addEventListener('click', toggleFavorite);
