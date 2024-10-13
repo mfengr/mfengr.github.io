9d6c196813a2bddf8bda7cd9794907ee582478ed
mfengr <mfengineeringplc@hotmail.com>
2024-10-13 22:21:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// The parent elements to add the 'Add to Favorites' button
-const degreeTitles = document.querySelectorAll(".card--nanodegree__title");
-
-console.log(degreeTitles.length);
-
-for(i=0;i<degreeTitles.length;i++){
-     const button = document.createElement('button');
-     button.classList = 'button button--primary';
-     button.textContent = 'Add to Favorites';
-     degreeTitles[i].appendChild(button);
-}
-
-
+document.addEventListener(‘DOMContentLoaded’, () => {
+  const newList = document.querySelectorAll(‘.card--nanodegree__title’);
+  newList.forEach(function (eve) {
+    const button = document.createElement('button');
+    button.textContent = 'Add to Favorites';
+    button.classList.add('button', 'button--primary');
+    eve.appendChild(button);
+  }
+});
