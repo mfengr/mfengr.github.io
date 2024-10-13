8e9b7a6e225a12980d435d13759f61f01dd2f0a4
mfengr <mfengineeringplc@hotmail.com>
2024-10-13 22:36:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-     const newList = document.querySelectorAll(‘.card--nanodegree__title’);
-  newList.forEach(function (eve) {
-    const button = document.createElement('button');
-    button.textContent = 'Add to Favorites';
-    button.classList.add('button', 'button--primary');
-    eve.appendChild(button);
-  }
+    console.log('the DOM is ready to be interacted with!');
 });
- 
-
-function toggleFavorite(event) {
-    const tagName = event.target;
-    if (tagName.nodeName === 'BUTTON') 
-    {
-        event.preventDefault();
-        if (tagName.textContent.startsWith('Add')) 
-        {
-            tagName.textContent = 'Remove from favorites';
-            const fav = document.createElement('p');
-            fav.textContent = '⭐ ⭐ ⭐ ';
-            tagName.parentElement.appendChild(fav);
-        } else {
-            tagName.textContent = 'Add to favorites';
-            if (tagName.nextSibling) 
-            {
-                tagName.nextSibling.remove();
-            }
-        }
-    }
-}
-
-const ndsContainer = document.querySelector('.list-nanodegrees');
-if (ndsContainer) {
-    ndsContainer.addEventListener('click', toggleFavorite);
-}
-});
-
